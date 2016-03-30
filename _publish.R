491acee9261979f32c9584b93fa894167c67a652
Yihui Xie <xie@yihui.name>
2016-03-30 17:17:17 -0500

    publish the gitbook format later than bootstrap/tufte so that it shows before them on the homepage

--- _publish.R
+++ _publish.R
@@ -1,20 +1,6 @@
 # publish the book with differnet HTML styles; you should not need this script
 
-# default formats
-formats = c(
-  'bookdown::pdf_book', 'bookdown::epub_book', 'bookdown::gitbook'
-)
-
 unlink('_book', recursive = TRUE)
-
-# render the book to all formats unless they are specified via command-line args
-for (fmt in formats) {
-  cmd = sprintf("bookdown::render_book('index.Rmd', '%s', quiet = TRUE)", fmt)
-  res = bookdown:::Rscript(c('-e', shQuote(cmd)))
-  if (res != 0) stop('Failed to compile the book to ', fmt)
-}
-
-bookdown::publish_book('bookdown-demo')
 
 x = readLines('index.Rmd')
 i = 1
@@ -39,3 +25,17 @@ for (fmt in c('html_book', 'tufte_html_book')) {
   bookdown::publish_book(paste0('bookdown-demo', i))
 }
 unlink('_book', recursive = TRUE)
+
+# default formats
+formats = c(
+  'bookdown::pdf_book', 'bookdown::epub_book', 'bookdown::gitbook'
+)
+
+# render the book to all formats unless they are specified via command-line args
+for (fmt in formats) {
+  cmd = sprintf("bookdown::render_book('index.Rmd', '%s', quiet = TRUE)", fmt)
+  res = bookdown:::Rscript(c('-e', shQuote(cmd)))
+  if (res != 0) stop('Failed to compile the book to ', fmt)
+}
+
+bookdown::publish_book('bookdown-demo')
