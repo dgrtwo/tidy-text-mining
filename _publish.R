772223e2a336f5922214206ae87827563e5b5592
Yihui Xie <xie@yihui.name>
2016-03-30 01:31:53 -0500

    get rid of _render.R, and remove html_book/tufte_html_book formats so the HTML formats won't overwrite each other under the _book directory; also tweaked _publish.R so I can still publish bootstrap/tufte styles to bookdown.org

--- _publish.R
+++ _publish.R
@@ -1,7 +1,19 @@
-# publish the book with other styles
+# publish the book with differnet HTML styles; you should not need this script
+
+# default formats
+formats = c(
+  'bookdown::pdf_book', 'bookdown::epub_book', 'bookdown::gitbook'
+)
 
 unlink('_book', recursive = TRUE)
-bookdown:::Rscript('_render.R')
+
+# render the book to all formats unless they are specified via command-line args
+for (fmt in formats) {
+  cmd = sprintf("bookdown::render_book('index.Rmd', '%s', quiet = TRUE)", fmt)
+  res = bookdown:::Rscript(c('-e', shQuote(cmd)))
+  if (res != 0) stop('Failed to compile the book to ', fmt)
+}
+
 bookdown::publish_book('bookdown-demo')
 
 x = readLines('index.Rmd')
@@ -10,12 +22,18 @@ s = paste0('title: "A Minimal Book Example (', c('Bootstrap', 'Tufte'), ' Style)
 for (fmt in c('html_book', 'tufte_html_book')) {
   unlink('_book', recursive = TRUE)
   file.copy('index.Rmd', '_index.Rmd')
+  file.copy('_output.yml', '_output.yml2')
   writeLines(
     gsub('^title: ".*"', s[i], gsub('gitbook', fmt, x)), 'index.Rmd'
+  )
+  cat(
+    'bookdown::', fmt, ':\n', '  css: [style.css, toc.css]\n', sep = '', file = '_output.yml',
+    append = TRUE
   )
   cmd = sprintf("bookdown::render_book('index.Rmd', 'bookdown::%s', quiet = TRUE)", fmt)
   res = bookdown:::Rscript(c('-e', shQuote(cmd)))
   file.rename('_index.Rmd', 'index.Rmd')
+  file.rename('_output.yml2', '_output.yml')
   if (res != 0) stop('Failed to compile the book to ', fmt)
   i = i + 1
   bookdown::publish_book(paste0('bookdown-demo', i))
