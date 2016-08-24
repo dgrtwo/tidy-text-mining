a57137572dad3e419074b5044c4886b287d2fc53
juliasilge <julia.silge@gmail.com>
2016-08-24 16:10:13 -0600

    We will try this, but I am skeptical. References #2.

---
+++ _deploy.sh
@@ -0,0 +1,19 @@
+# Set git config information
+git config --global user.name "David Robinson"
+git config --global user.email "admiral.david@gmail.com"
+
+# Clone the gh-pages repository
+git clone -b gh-pages \
+  https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git \
+  book-output
+
+# Change to the gh-page clone book-output directory
+cd book-output
+
+# Copy generated output to book-output
+cp -r ../_book/* ./
+
+# Add all files to the repo
+git add *
+git commit -a -m "Updating book (${TRAVIS_BUILD_NUMBER})"
+git push origin gh-pages
