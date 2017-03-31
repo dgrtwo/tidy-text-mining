62a11004e5b1c7533c9292046b42f7fa1076f1b1
juliasilge <julia.silge@gmail.com>
2017-03-31 16:20:41 -0600

    More regex! Regex is fun

--- convert_oreilly.R
+++ convert_oreilly.R
@@ -2,6 +2,9 @@ library(tidyverse)
 library(stringr)
 library(bookdown)
 
+
+# new branch -> change tips/notes/warnings (VERSION FOR EBOOK) -> new branch -> grayscale figures (VERSION FOR PRINT)
+
 # first run:
 # render_book("index.Rmd", output_format = word_document2(), clean = FALSE)
 
@@ -9,21 +12,28 @@ book <- read_lines("_main.utf8.md")
 
 s <- str_replace_all(book, "<a .*?>(.*)</a>", "\\1")
 s <- str_replace_all(s, " +\\[@.*?\\]", "")
+s <- str_replace_all(s, "<strong>|<\\/strong>", "")
+s <- str_replace_all(s, "\\(.*_files/figure-docx/", "\\(")
+s <- str_replace_all(s, "images/", "")
 s <- s[!str_detect(s, "^\\<\\!\\-\\-")]
-s <- tail(s, -(which(str_detect(s, "^# Intro"))[1] - 1))
+s <- tail(s, -(which(str_detect(s, "^# Preface"))[1] - 1))
 
-outfolder <- "tidy-text-mining-3-chapters"
+outfolder <- "text-mining-manuscript"
 dir.create(outfolder, showWarnings = FALSE)
-write_lines(s, file.path(outfolder, "tidy-text-mining.md"))
+write_lines(s, file.path(outfolder, "text-mining-with-r.md"))
 
 # hack, for now
-for (f in paste0(c("02-tidy-text", "03-sentiment-analysis"), "_files")) {
+for (f in paste0(c("01-tidy-text",
+                   "02-sentiment-analysis",
+                   "03-tf-idf",
+                   "04-word-combinations",
+                   "05-document-term-matrices",
+                   "06-topic-models",
+                   "07-tweet-archives",
+                   "08-nasa-metadata",
+                   "09-usenet"), "_files")) {
   out2 <- file.path(outfolder, f)
   dir.create(out2, showWarnings = FALSE)
   system(paste0("cp -r _bookdown_files/", f, "/figure-docx ", out2))
 }
 
-
-
-## on 2017-03-31, this worked:
-## render_book("index.Rmd", output_format = "pdf_book",clean=FALSE)
